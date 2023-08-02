ace2fed29ba48f12fd9f9d93e589c86b76d75d52
maakhu <egri@me.com>
2023-08-02 18:17:55 +0200

    add Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey, I am Bálint and I am going to teach you English!
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {/* Learn React */}
         </a>
       </header>
     </div>
---
+++ src/components/Testimonials.ts
@@ -0,0 +1,4 @@
+// A testimonial is effectively a review or recommendation from a client, 
+// letting other people know how your products or services benefitted them. 
+// Customer testimonials can take multiple forms, but the goal is always the same: 
+// to showcase how great your brand is.
